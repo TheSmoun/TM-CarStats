26abc12babe2a8af4494bc824ebddc104889748e
TheSmoun <simon.grossmann93@gmail.com>
2014-10-29 16:23:30 +0100

    Added loaded EventHandler.

--- TM-CarStats/MainWindow.xaml.cs
+++ TM-CarStats/MainWindow.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+using TMTVO.Api;
+using TMTVO.Data.Modules;
+using TMTVO_Modules.Data.Modules;
 
 namespace TM_CarStats
 {
@@ -20,9 +24,46 @@ namespace TM_CarStats
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static readonly int TICKS_PER_SECOND = 20;
+
+        public API Api { get; private set; }
+
+        private DispatcherTimer updateTimer;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Api = new API(TICKS_PER_SECOND);
+
+            Api.AddModule(new SessionsModule());
+            Api.AddModule(new SessionTimerModule());
+            Api.AddModule(new TeamRadioModule());
+            Api.AddModule(new DriverModule());
+            Api.AddModule(new LiveStandingsModule());
+            Api.AddModule(new CameraModule());
+            Api.AddModule(new TimeDeltaModule());
+            Api.AddModule(new GridModule());
+            Api.AddModule(new IRatingModule());
+
+            updateTimer = new DispatcherTimer();
+            updateTimer.Interval = TimeSpan.FromMilliseconds(50);
+            updateTimer.Tick += Api.Connect;
+            updateTimer.Tick += updateStatusBar;
+
+
+            updateTimer.Start();
+        }
+
+        private void updateStatusBar(object sender, EventArgs e)
+        {
+            if (Api.IsConnected)
+                Item1.Content = "iRacing simulatior connected.";
+            else
+                Item1.Content = "iRacing simulatior not connected.";
         }
     }
 }
